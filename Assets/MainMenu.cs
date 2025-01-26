606f78b403caf70bef7774061b314749c194f66e
Laura Klein <86952623+Laurakle@users.noreply.github.com>
2025-01-26 16:35:19 +0100

    code changer

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
  public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("Final Scene");
+        SceneManager.LoadSceneAsync("LE JEU");
     }
  
 }
