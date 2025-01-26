34203e685aabf92c72143cda78bf8d83ddbe1a9b
Paul Labaye <paul.labaye@gmail.com>
2025-01-26 12:51:48 +0100

    added triggerzone + enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,14 +13,26 @@ public class Enemy : MonoBehaviour
     
     private void Update()
     {
-        if (m_Aggro)
-            transform.position = Vector3.MoveTowards(transform.position, Player.Instance.transform.position, m_Speed);
+        if (!m_Aggro) return;
+        
+        transform.position = Vector3.MoveTowards(transform.position, Player.Instance.transform.position, m_Speed);
+        Orientate(Player.Instance.transform.position);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void Orientate(Vector3 target)
     {
-        if (other.gameObject.GetComponent<Player>() == null) return;
+        float rotZ = Mathf.Atan2(target.y, target.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ + 180);
+    }
 
+    public void Aggro()
+    {
         m_Aggro = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>() == null) return;
+        Player.Instance.Die();
     }
 }
---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public Enemy[] m_Enemies;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<Player>() == null) return;
+        foreach (Enemy enemy in m_Enemies) {
+            enemy.Aggro();
+        }
+    }
+}
